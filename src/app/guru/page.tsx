f268919892f2b246eae0ab05214ccfe70363e464
fajarsembar01 <fajarsembar@gmail.com>
2025-06-24 10:56:52 +0700

    perbuahan data

--- src/app/guru/page.tsx
+++ src/app/guru/page.tsx
@@ -99,7 +99,7 @@ export default function GuruPage() {
                     <Search className="absolute left-2.5 top-2.5 w-4 h-4 text-gray-400" />
                     <input
                         type="text"
-                        placeholder="Cari nama atau email..."
+                        placeholder="Cari nama"
                         className="pl-8 pr-3 py-2 w-full border rounded-md text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-100"
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
@@ -145,7 +145,7 @@ export default function GuruPage() {
                         <Search className="absolute left-2.5 top-2.5 w-4 h-4 text-gray-400" />
                         <input
                             type="text"
-                            placeholder="Cari nama atau email..."
+                            placeholder="Cari nama"
                             className="pl-8 pr-3 py-2 w-full border rounded-md text-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-100"
                             value={search}
                             onChange={(e) => setSearch(e.target.value)}
@@ -191,18 +191,14 @@ export default function GuruPage() {
                                 </div>
 
                                 {guru.nip && (
-                                    <div className="absolute bottom-10 left-3 group-hover:opacity-0 transition duration-300">
-                                        <Badge className="bg-amber-100 text-amber-600 text-xs font-medium px-3 py-1 rounded-md">
-                                            NIP: {guru.nip}
+                                    <div className="absolute bottom-3 left-3 group-hover:opacity-0 transition duration-300">
+                                    <Badge className="bg-indigo-100 text-indigo-600 text-xs font-medium px-3 py-1 rounded-md hover:bg-indigo-200 transition">
+                                       NIP: {guru.nip}
                                         </Badge>
                                     </div>
                                 )}
 
-                                <div className="absolute bottom-3 left-3 group-hover:opacity-0 transition duration-300">
-                                    <Badge className="bg-indigo-100 text-indigo-600 text-xs font-medium px-3 py-1 rounded-md hover:bg-indigo-200 transition">
-                                        <a href={`mailto:${guru.email}`}>{guru.email}</a>
-                                    </Badge>
-                                </div>
+                               
                             </div>
 
                             <div className="pt-2 px-4 pb-4 space-y-2">
@@ -214,11 +210,7 @@ export default function GuruPage() {
                                 <div className="text-sm flex items-center gap-2">
                                     <Calendar className="w-4 h-4 text-gray-500" />
                                     {guru.tempat_lahir},{" "}
-                                    {new Date(guru.tanggal_lahir).toLocaleDateString("id-ID", {
-                                        day: "numeric",
-                                        month: "long",
-                                        year: "numeric",
-                                    })}
+                                    
                                 </div>
 
                                 {guru.nuptk && (
